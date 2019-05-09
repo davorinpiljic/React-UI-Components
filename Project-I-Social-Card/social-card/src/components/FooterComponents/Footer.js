9cf93c7fce4e434b1ec7dfdecb154fa14b260a95
davorin <47928470+davorinpiljic@users.noreply.github.com>
2019-05-08 21:03:24 -0600

    header added, card added, footer without icons added

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return(
+    <div className="footer">
+        <img src="{}" alt="chat" />;
+        <img src="{}" alt="refreshy" />;
+        <img src="{} "alt="like" />;
+        <img src="{}" alt="mail" />;
+    </div>
+
+    )
+}
+
+export default Footer
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,32 @@
 import React from 'react';
 import './Header.css';
+
+import ImageThumbnail from './ImageThumbnail'
+import HeaderTitle from './HeaderTitle'
+import HeaderContent from './HeaderContent'
+
+const HeaderContainer = () => {
+
+    return(
+        <div className="header-container">
+
+            <div className="header-image">
+                <ImageThumbnail />
+            </div>
+        
+            <div className="header-content">
+                <div>
+                    <HeaderTitle />
+                </div>
+                
+                <div>
+                    <HeaderContent />
+                </div>   
+            </div>
+
+        </div>
+    )
+}
+
+
+export default HeaderContainer
